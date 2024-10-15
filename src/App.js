c1171a36777b5c5c30478c442225ef313c65e6ad
cashbarnes <cashmbarnes@gmail.com>
2024-10-15 08:47:26 -0700

    Added conditional rendering for query box

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import DataList from './components/DataList';
 
 const App = () => {
   const [query, setQuery] = useState(''); // State for user input to query data
-  const [showResults, setShowResults] = useState(true); // State to control initial display of results
+  const [showResults, setShowResults] = useState(false); // State to control display of results
+  const [showQueryBox, setShowQueryBox] = useState(false); // State to control query box visibility
   const { data, storedData, setData, error } = useFetchData();
 
   // Function to filter stored data based on query input
@@ -23,7 +24,6 @@ const App = () => {
     const input = e.target.value;
     setQuery(input);
 
-    // Only show results after a query is made
     if (input.length > 0) {
       setShowResults(true);
       setData(queryData(input)); // Update displayed data based on query
@@ -37,8 +37,10 @@ const App = () => {
       <h1>Stardog Query Results</h1>
       {error && <p>Connection failed: {error}</p>}
 
-      {/* Query Input Component */}
-      <QueryInput query={query} handleQueryChange={handleQueryChange} />
+      {/* Conditionally render the QueryInput component based on showQueryBox */}
+      {showQueryBox && (
+        <QueryInput query={query} handleQueryChange={handleQueryChange} />
+      )}
 
       {/* Conditionally render the DataList only if showResults is true */}
       {showResults && <DataList data={data} />}
