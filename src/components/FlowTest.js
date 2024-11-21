2ec1d04458c89377a1e3419640548d507af0cbc8
kroth-ultra <kroth@ultranauts.co>
2024-11-21 14:34:15 -0600

    Updating FlowTest.js with some code changes which display the nodes and edges we have for React Flow. Also adding some very basic logic to move the nodes programmatically and show something slightly nicer than all of them rendering on top of each other. There is a lot of room to improve, but this is a starting place.

--- src/components/FlowTest.js
+++ src/components/FlowTest.js
@@ -3,7 +3,7 @@ import React, {useState, useCallback} from "react";
 import useFetchData from "./useFetchData";
 
 function Dashboard() {
-    const { nodeData, edgeData } = useFetchData();
+    const { nodeData, setNodeData, edgeData, setEdgeData } = useFetchData();
     // const initialNodes = [
     // {id: '1', position: {x: 0, y: 0}, data: {label: '1'}},
     // {id: '2', position: {x: 0, y: 100}, data: {label: '2'}},
@@ -13,14 +13,31 @@ function Dashboard() {
     //     {id: 'e1-2', source: '1', target: '2'},
     //     {id: 'e2-3', source: '2', target: '3'},
     // ];
-    const [nodes, setNodes] = useState(nodeData);
+
+    let systemCount = 0, reportCount = 0;
+
+    for (let i = 0; i < nodeData.length; i++)
+    {
+      if (nodeData[i].id.toLowerCase().endsWith('report'))
+      {
+        nodeData[i].position.x = 300;
+        nodeData[i].position.y = reportCount * 100;
+        reportCount++;
+      }
+      else
+      {
+        nodeData[i].position.y = systemCount * 100;
+        systemCount++;
+      }
+    }
+    
     const onNodesChange = useCallback(
-  (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-  [setNodes]
+      (changes) => setNodeData((nds) => applyNodeChanges(changes, nds)),
+      [setNodeData]
     );
   return (
         <div className='relation-section' style={{ width:800, height:400 }}>
-        <ReactFlow nodes={nodes} edges={edgeData} onNodesChange={onNodesChange}>
+        <ReactFlow nodes={nodeData} edges={edgeData} onNodesChange={onNodesChange}>
         <MiniMap />
         <Controls />
         </ReactFlow>
