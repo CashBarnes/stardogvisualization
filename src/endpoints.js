0c80f864bb44799a98a0c36d6836b39ac6e2d004
cashbarnes <cashmbarnes@gmail.com>
2025-01-02 13:42:15 -0800

    New endpoint created

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,2 +1,2 @@
-export const STARDOG_URL = 'https://sd-f63422c5.stardog.cloud:5820/KnowledgeGraphDemo/query';
-export const STARDOG_UPDATE = 'https://sd-f63422c5.stardog.cloud:5820/KnowledgeGraphDemo/update';
+export const STARDOG_URL = 'https://sd-f2818bb4.stardog.cloud:5820/KnowledgeGraph1bDemo/query';
+export const STARDOG_UPDATE = 'https://sd-f2818bb4.stardog.cloud:5820/KnowledgeGraph1bDemo/update';
